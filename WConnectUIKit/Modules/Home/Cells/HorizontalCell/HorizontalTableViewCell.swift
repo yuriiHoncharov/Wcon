4f15f862a8a63602d1d4909f018dc076d24fef7a
yuriiHoncharov <y.honcharov@lampalampa.net>
2023-02-24 11:47:38 +0200

    qq

---
+++ WConnectUIKit/Modules/Home/Cells/HorizontalCell/HorizontalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HorizontalTableViewCell.swift
+//  WConnectUIKit
+//
+//  Created by Yurii Honcharov on 24.02.2023.
+//
+
+import UIKit
+
+class HorizontalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
